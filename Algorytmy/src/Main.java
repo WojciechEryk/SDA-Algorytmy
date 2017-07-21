066595ebe2e4d31794243cc324c2e50bd4a0f969
Wojt <w.g.michalik@gmail.com>
2017-07-21 19:46:47 +0200

    Fabryka PUSH

--- Algorytmy/src/Main.java
+++ Algorytmy/src/Main.java
@@ -1,6 +1,23 @@
+import Alghoritms.AbstractAlgorithm;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    public static void main (String[] args) throws Exception {
+        String algorithmName = args [0];
+
+        for (int i=0; i<args.length; i++){
+            System.out.println(args[i]);}
+
+        System.out.println("----------------");
+
+        for (String s:args) {
+            System.out.println(s);}
+
+        AlgorithmFactory factory = new AlgorithmFactory();
+        AbstractAlgorithm algorithm =
+                factory.getAlgorithm(algorithmName);
+
+
+        }
 }
+
